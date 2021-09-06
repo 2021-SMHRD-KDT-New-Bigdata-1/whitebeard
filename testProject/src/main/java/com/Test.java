0aae68f9c16f56cf1e455a70e91235672eb6ef4b
smhrd <smhrd@220.95.45.253>
2021-09-06 16:11:05 +0900

    두 번째 커밋!

--- testProject/src/main/java/com/Test.java
+++ testProject/src/main/java/com/Test.java
@@ -1,9 +1,10 @@
 package com;
 
-public class test {
+public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("두 번째 커밋!"); 
 	}
 
 }
