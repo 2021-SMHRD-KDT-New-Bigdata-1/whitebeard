46e4576cfc1d8c027265ef12e08cab3677688578
smhrd <smhrd@211.227.114.190>
2021-09-24 15:30:37 +0900

    memberDAO 생성, JoinService dao로 수정

---
+++ testProject0923/src/main/java/com/DAO/memberDAO.java
@@ -0,0 +1,70 @@
+package com.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class memberDAO {
+
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs =null;
+	
+	public void conn() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String dbid = "cgi_6_2";
+			String dbpw = "smhrd2";
+			conn = DriverManager.getConnection(url, dbid, dbpw);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		try {
+			if(rs!=null) {
+				rs.close();
+			}
+			if(psmt!=null) {
+				psmt.close();
+			}
+			if(conn!=null) {
+				conn.close();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public int join(String id, String pw, String name, String date, String nick, String email, String phone) {
+		int cnt = 0;
+		try {
+			conn();
+		
+			String sql = "insert into members(member_id, member_pw, name, birth_date, nickname, email, phone, member_type) values(?, ?, ?, ?, ?, ?, ?, 0)";
+		
+			PreparedStatement psmt = conn.prepareStatement(sql);
+			
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+			psmt.setString(3, name);
+			psmt.setString(4, date);		
+			psmt.setString(5, nick);
+			psmt.setString(6, email);
+			psmt.setString(7, phone);
+			
+			cnt = psmt.executeUpdate();  
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return cnt;
+	}
+	
+	
+}
