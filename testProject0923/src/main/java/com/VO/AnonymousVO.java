4315abe1691fe4797347d040985f3e67fcc2c56c
smhrd <smhrd@220.95.45.253>
2021-09-24 11:01:54 +0900

    VO추가추가

---
+++ testProject0923/src/main/java/com/VO/AnonymousVO.java
@@ -0,0 +1,16 @@
+package com.VO;
+
+import java.sql.Date;
+
+public class AnonymousVO {
+
+	private int ano_seq;
+	private String member_id;
+	private String ano_subject;
+	private String ano_content;
+	private String ano_pic1;
+	private String ano_pic2;
+	private String ano_pic3;
+	private Date date;
+	
+}
