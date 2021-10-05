ac6dc6f0473beb2ee12a38706824e60c37472947
smhrd <smhrd@211.227.114.190>
2021-10-05 19:34:20 +0900

    판매자 가입 시 세션값 수정, 마이페이지 일반회원 수정창 변경

--- testProject0923/src/main/java/com/servlet/SellerJoinService.java
+++ testProject0923/src/main/java/com/servlet/SellerJoinService.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.DAO.memberDAO;
+import com.VO.MemberVO;
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
@@ -50,14 +51,28 @@ public class SellerJoinService extends HttpServlet {
 //		String company_pic3 = request.getParameter("company_pic3");
 //		String company_info = request.getParameter("company_info");
 		
-		System.out.println(member_id + ","+member_pw + ","+ company_name + ","+ company_bn + ","+ b_type + ","+ company_pic1 + ","+ company_pic2 + ","+ company_pic3 + ","+ company_info);
 		memberDAO dao = new memberDAO();
+		HttpSession session = request.getSession(); 
+		MemberVO vo = (MemberVO)session.getAttribute("vo");
+		String name = vo.getName();
+		String date = vo.getBirth_date();
+		String nick = vo.getNickname();
+		String email = vo.getEmail();
+		String phone = vo.getPhone();
+		String member_type = vo.getMember_type();
+		
 		int cnt = dao.sellerjoin(member_id, member_pw, company_name, company_bn, b_type, company_pic1, company_pic2, company_pic3, company_info);
-		System.out.println("서블릿" + cnt);
-		if(cnt>0) {
-			response.setCharacterEncoding("euc-kr");
-			response.setContentType("text/html; charset=euc-kr");
-			PrintWriter out = response.getWriter();
+		
+		response.setCharacterEncoding("euc-kr");
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter out = response.getWriter();
+		
+		if(cnt > 0) {
+			MemberVO vo2 = new MemberVO(member_id, member_pw, name, date, nick, email, phone, member_type, company_name,
+					company_bn, b_type, company_pic1, company_pic2, company_pic3, company_info);
+			session.setAttribute("vo", vo2);
+			
+			
 			out.print("<script language='javascript' charset='euc-kr'>");
 			out.print("alert('판매자 등록 완료!');");
 			out.print("location.href='main.jsp'");
@@ -65,10 +80,6 @@ public class SellerJoinService extends HttpServlet {
 		}
 			
 		else{
-			
-			response.setCharacterEncoding("euc-kr");
-			response.setContentType("text/html; charset=euc-kr");
-			PrintWriter out = response.getWriter();
 			out.print("<script language='javascript' charset='euc-kr'>");
 			out.print("alert('아이디나 비밀번호가 올바르지 않습니다.');");
 			out.print("location.href='sellerjoin.html'");
